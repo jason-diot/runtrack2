8cbc7f7d8f505737e4a583be4e4899ec1737328e
Jason <jason.diot@laplateforme.io>
2023-05-29 10:24:36 +0200

    commit

---
+++ jour06/job05/index.php
@@ -0,0 +1,19 @@
+<?php
+function occurrences($str, $char) {
+    $count = 0;
+    $length = strlen($str);
+
+    for ($i = 0; $i < $length; $i++) {
+        if ($str[$i] === $char) {
+            $count++;
+        }
+    }
+
+    return $count;
+}
+$str = "Bonjour";
+$char = "o";
+
+$occurrences = occurrences($str, $char);
+echo $occurrences; 
+?>
